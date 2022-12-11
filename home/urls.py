364cdaa75a4224c7bc780c52ced300e4bb629d66
mohd-fa <91002304+mohd-fa@users.noreply.github.com>
2022-12-11 13:02:20 +0530

    navigation-admin

--- home/urls.py
+++ home/urls.py
@@ -2,8 +2,11 @@
 from django.urls import path
   
 # importing views from views..py
-from .views import index
+from .views import home,about,events,archive
   
 urlpatterns = [
-    path('', index ),
+    path('', home ),
+    path('about', about ),
+    path('events', events ),
+    path('archive', archive ),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(request , 'index.html')
+def home(request):
+    return render(request , 'home.html', {'active': 'home'})
+
+def events(request):
+    return render(request , 'events.html', {'active': 'events'})
+
+def archive(request):
+    return render(request , 'archive.html', {'active': 'archive'})
+
+def about(request):
+    return render(request , 'about.html', {'active': 'about'})
